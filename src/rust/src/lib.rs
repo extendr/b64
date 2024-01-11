9a70f5e879252c58db1d8d04af1724705aeeacc7
Josiah Parry <josiah.parry@gmail.com>
2024-01-11 11:36:40 -0500

    scaffold more customizability

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,6 +1,6 @@
 use extendr_api::prelude::*;
-use base64::prelude::*;
-use base64::engine::general_purpose;
+use base64::{prelude::*, alphabet, engine};
+use base64::engine::{general_purpose, DecodePaddingMode, GeneralPurposeConfig};
 use std::fs;
 use itertools::Itertools;
 
@@ -11,9 +11,7 @@ use itertools::Itertools;
 /// @name encode
 #[extendr]
 fn encode_string(input: String) -> String {
-    let mut buf = String::new();
-    general_purpose::STANDARD.encode_string(input,  &mut buf);
-    buf
+    general_purpose::STANDARD.encode(input)
 }
 
 
@@ -21,25 +19,19 @@ fn encode_string(input: String) -> String {
 /// @name encode
 #[extendr]
 fn encode_raw(input: Raw) -> String {
-    let mut buf = String::new();
-    general_purpose::STANDARD.encode_string(input.as_slice(),  &mut buf);
-    buf
+    general_purpose::STANDARD.encode(input.as_slice())
 }
 
 /// @export
 /// @name encode
 #[extendr]
 fn encode_file(path: String) -> String {
-    let mut buf = String::new();
     let fp = fs::read(&path);
-
     if let Ok(contents) = fp {
-        general_purpose::STANDARD.encode_string(&contents, &mut buf);
+        general_purpose::STANDARD.encode(&contents)
     } else {
         extendr_api::throw_r_error("Unable to read file.")
     }
-    
-    buf
 }
 
 #[extendr]
@@ -57,7 +49,7 @@ fn chunk_encoding(encoded: String, size: i32) -> Strings {
 }
 
 #[extendr]
-fn collapse_chunks(chunks: Strings, newline: &str) -> String {
+fn line_wrap(chunks: Strings, newline: &str) -> String {
     chunks.into_iter().join(newline)
 }
 
@@ -87,6 +79,87 @@ fn decode_raw(input: Raw) -> Vec<u8> {
     }
 }
 
+// use a built-in alphabet
+#[extendr]
+fn alphabet_(which: &str) -> ExternalPtr<alphabet::Alphabet> {
+    match which {
+        "bcrypt" => ExternalPtr::new(alphabet::BCRYPT),
+        "bin_hex" => ExternalPtr::new(alphabet::BIN_HEX),
+        "crypt" => ExternalPtr::new(alphabet::CRYPT),
+        "imap_mutf7" => ExternalPtr::new(alphabet::IMAP_MUTF7),
+        "standard" => ExternalPtr::new(alphabet::STANDARD),
+        "url_safe" => ExternalPtr::new(alphabet::URL_SAFE),
+        _ => extendr_api::throw_r_error(&format!("Unknown alphabet: {}", which))
+    }
+}
+
+// Create new alphabet
+#[extendr]
+fn new_alphabet(chars: &str) ->  ExternalPtr<alphabet::Alphabet> {
+    let res = alphabet::Alphabet::new(chars).unwrap();
+    ExternalPtr::new(res)
+}
+
+#[extendr]
+fn print_alphabet(alphabet: Robj) {
+    let alph: ExternalPtr<alphabet::Alphabet> = alphabet.try_into().unwrap();
+    println!("{:?}", &alph);
+}
+
+// default configs 
+// padding = true, 
+// decode_allow_trailing_bits = false, 
+// and decode_padding_mode = DecodePaddingMode::RequireCanonicalPadding
+#[extendr]
+fn new_config_(
+    encode_padding: bool,
+    decode_padding_trailing_bits: bool,
+    decode_padding_mode: &str,
+) -> ExternalPtr<GeneralPurposeConfig> {
+    let pad_mode = match decode_padding_mode {
+        "indifferent" => DecodePaddingMode::Indifferent,
+        "canonical" => DecodePaddingMode::RequireCanonical,
+        "none" => DecodePaddingMode::RequireNone,
+        _ => extendr_api::throw_r_error(&format!("Unknown padding mode: {}", decode_padding_mode))
+    };
+
+    let config = GeneralPurposeConfig::new()
+        .with_encode_padding(encode_padding)
+        .with_decode_allow_trailing_bits(decode_padding_trailing_bits)
+        .with_decode_padding_mode(pad_mode);
+
+    ExternalPtr::new(config)
+}
+
+
+/// Create base64 Engines
+/// 
+/// `engine()` creates a new `GeneralPurpose` engine. 
+/// 
+/// @param
+/// 
+/// @export
+#[extendr]
+fn engine_(which: &str) -> ExternalPtr<engine::GeneralPurpose> {
+    match which {
+        "standard" => ExternalPtr::new(general_purpose::STANDARD),
+        "standard_no_pad" => ExternalPtr::new(general_purpose::STANDARD_NO_PAD),
+        "url_safe" => ExternalPtr::new(general_purpose::URL_SAFE),
+        "url_safe_no_pad" => ExternalPtr::new(general_purpose::URL_SAFE_NO_PAD),
+        _ => extendr_api::throw_r_error(&format!("Unknown engine: {}", which))
+    }
+}
+
+#[extendr]
+fn new_engine_(alphabet: Robj, config: Robj) -> ExternalPtr<engine::GeneralPurpose> {
+    let alph: ExternalPtr<alphabet::Alphabet> = alphabet.try_into().unwrap();
+    let conf: ExternalPtr<GeneralPurposeConfig> = config.try_into().unwrap();
+    let inner = conf.addr();
+    let engine = general_purpose::GeneralPurpose::new(&alph, *conf);
+    ExternalPtr::new(engine)
+}
+
+
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
@@ -99,8 +172,14 @@ extendr_module! {
     // decoding
     fn decode_string;
     fn decode_raw;
+    
     // helpers
     fn chunk_encoding;
-    fn collapse_chunks;
+    fn line_wrap;
+
+    // alphabets
+    fn alphabet_;
+    fn new_alphabet;
+    fn print_alphabet;
 }
 
