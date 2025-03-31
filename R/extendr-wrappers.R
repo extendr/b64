22869aade0fd8bf7c98096fc55f943b3988447b4
Josiah Parry <josiah.parry@gmail.com>
2025-03-31 08:26:03 -0700

    bump to 0.8.0 and devel rextend

--- R/extendr-wrappers.R
+++ R/extendr-wrappers.R
@@ -6,7 +6,6 @@
 # This file was created with the following call:
 #   .Call("wrap__make_b64_wrappers", use_symbols = TRUE, package_name = "b64")
 
-#' @docType package
 #' @usage NULL
 #' @useDynLib b64, .registration = TRUE
 NULL
